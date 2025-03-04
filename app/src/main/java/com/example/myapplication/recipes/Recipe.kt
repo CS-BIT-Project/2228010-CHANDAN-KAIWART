a6aa64441a00c4937c09b87110eebc98530f2d4a
Chandan Kaiwart <kaiwartji2020@gmail.com>
2025-03-05 01:35:41 +0530

    update1

--- app/src/main/java/com/example/myapplication/recipes/Recipe.kt
+++ app/src/main/java/com/example/myapplication/recipes/Recipe.kt
@@ -3,6 +3,7 @@ package com.example.myapplication.recipes
 import com.google.gson.annotations.SerializedName
 
 data class Recipe(
+    @SerializedName("id") val id: Int,
     @SerializedName("title") val title: String,
     @SerializedName("readyInMinutes") val readyInMinutes: Int,
     @SerializedName("spoonacularScore") val spoonacularScore: Float?,
